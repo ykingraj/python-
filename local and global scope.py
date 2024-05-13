c363c3c45ebb725fea3fe31b383d628cf22307d0
ykingraj <136958320+ykingraj@users.noreply.github.com>
2024-05-13 02:28:04 +0000

    something

--- local and global scope.py
+++ local and global scope.py
@@ -2,20 +2,50 @@ x = 20
 
 def function1 ():
     x1 = 40
-    print (x)
+    x = "changed"
+    print (x , " Local function 1 call")
+    print (x1)
+
+    def function2 ():
+        #nonlocal x1 
+        x1 = 44
+        x2 = 50
+        print (x , "in fun 2")
+        print (x1 , " x1 in fun 2")
+        print (x2)
+
+    
+    function2()
+    print (x1 ," x1 after fun2")
+    
+    x = "again changed"
+    print (x , " after fun2")
+    
+
+
+function1()
+print (x, " globel call")
+#print (x1)
+
+'''
+x = 20
+
+def function1 ():
+    x1 = 40
+    global x 
+    x = "changed"
+    print (x , " Local function 1 call")
     print (x1)
 
     def function2 ():
         #nonlocal x2 
         x2 = 50
-        print (x)
-        print (x1)
-        print (x2)
-
+        #print (x)
+        #print (x1)
+        #print (x2)
     function2()
 
-function1()
 
-print (x)
-#print (x1)
-#print (x2)
+function1()
+print (x, " globel call")
+'''
